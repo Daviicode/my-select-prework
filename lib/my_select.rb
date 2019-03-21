1c19583da280473e5eb29d31d66d695b4814ad62
David zhang <d.ydzdavid@yahoo.com>
2019-03-21 23:57:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,6 @@
 def my_select(collection)
   new_collection = []
-  collection.select(num) do {|num| new_collection << yield(collection[num])}
+  collection.select do {|num| new_collection << yield(collection[num])}
 end
 new_collection
 end
